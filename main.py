747f334711c62d57359921df0ea1fdfcc49440d5
tgloyn <troy.gloyn@gmail.com>
2025-06-11 13:49:29 -0400

    created new main.py file

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def main():
+    st.title("Welcome to My Streamlit App")
+    st.write("This is a simple Streamlit application.")
+    st.button("Click Me")
+
+
+if __name__ == "__main__":
+    main()
+    

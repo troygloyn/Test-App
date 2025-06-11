f6bd2430e151d056f232ec7ce03b25f5c3820ad0
tgloyn <troy.gloyn@gmail.com>
2025-06-11 14:59:14 -0400

    updated main.py

--- main.py
+++ main.py
@@ -28,18 +28,18 @@ def main():
         with st.chat_message('user'):
             st.markdown(prompt)
 
-    with st.chat_message('assistant'):
-        stream = client.chat.completions.create(
-            model=st.session_state['openai_model'],
-            messages=[
-                {'role': m['role'], 'content': m['content']}
-                for m in st.session_state.messages
-            ],
-            stream=True
-        )
-        response = st.write_stream(stream)
+        with st.chat_message('assistant'):
+            stream = client.chat.completions.create(
+                model=st.session_state['openai_model'],
+                messages=[
+                    {'role': m['role'], 'content': m['content']}
+                    for m in st.session_state.messages
+                ],
+                stream=True
+            )
+            response = st.write_stream(stream)
 
-    st.session_state.messages.append({'role': 'assistant', 'content': response})
+        st.session_state.messages.append({'role': 'assistant', 'content': response})
 
 if __name__ == "__main__":
     main()
